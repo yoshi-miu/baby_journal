80ab77df2742d1d0901b3cb9d894455670279866
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-09-02 12:01:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  validates :name, presence: true
+  validates :password, format: { with: PASSWORD_REGEX }
 end
