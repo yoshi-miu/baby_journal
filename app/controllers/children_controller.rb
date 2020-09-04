fb2619cd3788280ff279f443bf45c7e87f8cf54d
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-09-04 16:06:39 +0900

    child詳細ページ

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,4 +1,5 @@
 class ChildrenController < ApplicationController
+before_action :set_child, only: [:show]
   def index
     if user_signed_in?
       @children = current_user.children
@@ -19,10 +20,28 @@ class ChildrenController < ApplicationController
     end
   end
 
+  def show
+    get_times
+  end
+
   private
 
   def child_params
     params.require(:child).permit(:name, :birth_day).merge(user_id: current_user.id)
   end
+
+  def set_child
+    @child = Child.find(params[:id])
+  end
+
+  def get_times
+    @t = Time.current
+    @times = []
+    t = Time.current
+    i = 22
+    25.times do |j|
+      @times << t.ago(i.hours)
+      i -= 1
+    end
   end
 end
