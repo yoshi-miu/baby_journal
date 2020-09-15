1a5db043ff6051cad2d0741edeaa21182c4be0ba
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-09-15 16:10:57 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'children#index'
   resources :children do
-    resources :breasts, only: [:new,:create]
+    resources :breasts, only: [:new, :create, :edit, :update]
   end
 end
